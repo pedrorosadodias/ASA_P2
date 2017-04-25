350faff225904d8c2a624c14d219953c0e1ef59a
Pedro Dias <pedro.rosado.dias@gmail.com>
2017-04-25 18:48:46 +0100

    tua versão enviada por e-mail

--- proj.c
+++ proj.c
@@ -16,17 +16,18 @@ typedef struct connection {
 } *Pconnection;
 
 
-static Pcity *graph;    /*pointer for the table of cities*/
 int NCities;            /*number of cities*/
 int NmaxAirports;       /*number max of airports*/
 int NmaxRoads;          /*number max of roads*/
 int TotalCost;          /*total cost*/
 int NRoads;             /*number of roads -> output*/
 int NAirports;          /*number of airports -> output*/
-int Airports[];
+int r, l;
 
 /*  Headers  */
 Pconnection newConnection (int city1, int city2, int cost);
+int cmpfunction(const void* a, const void* b);
+
 
 
 int main (){
@@ -36,82 +37,86 @@ int main (){
     NRoads = 0;
 
 
-    int i, city1, city2, cost;
+    int i, j, k, city1, city2, cost;
 
     scanf("%d",&NCities);
     scanf("%d",&NmaxAirports);
-        int Airports[NCities]; /*vetor dos aeroportos de cada cidade*/
-        int VecSize = ((NmaxAirports - 1) * NmaxAirports)/2;
-        Pconnection AirCosts[VecSize]; /*vetor dos custos de construção de aeroportos entre duas cidades*/
+    
+    int Airports[NCities]; /*vetor dos aeroportos de cada cidade*/
+    int AirCitys[NmaxAirports]; /*vetor das cidades que tem aeorportos*/
+    int VecSize = ((NmaxAirports - 1) * NmaxAirports)/2;
+    Pconnection* AirCosts = (Pconnection*) malloc(sizeof(struct connection) * VecSize); /*vetor dos custos de construção de aeroportos entre duas cidades*/
+
 
     for (i = 0; i < NmaxAirports; i++){
         scanf("%d %d", &city1, &cost);
         Airports[city1 - 1] = cost;
+        AirCitys[i] = city1;
         /* city1-> aeroporto na cidade city1   /   cost->custo de construção do aeroporto na cidade city1*/
     }
 
     scanf("%d",&NmaxRoads);
-        Pconnection RoadCosts[NmaxRoads];
+    Pconnection* RoadCosts = (Pconnection*) malloc(sizeof(struct connection) * NmaxRoads);
 
     for (i = 0; i < NmaxRoads; i++){
         scanf("%d %d %d", &city1, &city2, &cost);
             Pconnection new = newConnection(city1, city2, cost);
             RoadCosts[i] = new;
+            
 
-
-        /* city1-> cidade1  /   city2-> cidade2   /   cost->custo de construção da estrada entre city1 e city2*/
+       /* city1-> cidade1  /   city2-> cidade2   /   cost->custo de construção da estrada entre city1 e city2*/
     }
+
+
+
+
+    if (NmaxAirports > 1){
+    	k = VecSize;
+    	for(i = 0; i < NmaxAirports - 1; i++){
+    		for(j = i + 1; j < NmaxAirports; j++){
+    			int pos1 = AirCitys[i] - 1;
+    			int pos2 = AirCitys[j] - 1;
+    			int cost = Airports[pos1] + Airports[pos2];
+    			Pconnection new = newConnection(AirCitys[i], AirCitys[j], cost);
+    			AirCosts[--k] = new;
+    		}
+    	}
+    }
+
+for(i=0; i<NmaxAirports; i++){
+   printf("city1: %d city2: %d cost: %d\n", AirCosts[i]->city1, AirCosts[i]->city2, AirCosts[i]->cost);
+}
+
+for(i=0; i<NmaxRoads; i++){
+   printf("city1: %d city2: %d cost: %d\n", RoadCosts[i]->city1, RoadCosts[i]->city2, RoadCosts[i]->cost);
+}
+
+    qsort(RoadCosts, NmaxRoads, sizeof(Pconnection), cmpfunction);
+    qsort(AirCosts, NmaxAirports, sizeof(Pconnection), cmpfunction);
+
+for(i=0; i<NmaxAirports; i++){
+   printf("city1: %d city2: %d cost: %d\n", AirCosts[i]->city1, AirCosts[i]->city2, AirCosts[i]->cost);
+}
+
+for(i=0; i<NmaxRoads; i++){
+   printf("city1: %d city2: %d cost: %d\n", RoadCosts[i]->city1, RoadCosts[i]->city2, RoadCosts[i]->cost);
+}   
+
+
 
 
     return 0;
 }
 
 
+int cmpfunction(const void* a, const void* b){
 
+	const Pconnection p1 = *(const Pconnection *)a;
+	const Pconnection p2 = *(const Pconnection *)b;
 
-
-
-
-/*  Esquecer a partir daqui, mas n tirar pode ser necessário    */
-
-
-
-/*  Initializations of Table & vector of final order -> sources  */
-void initData(int n){
-    int i;
-    graph = (node*) malloc(sizeof(node) * n);
-    order = (int*) malloc(sizeof(int) * n);
-    for(i = 0; i < n; i++){
-        graph[i] = newPhoto(i + 1);
-        order[i] = 0;
-    }
+	return(p1->cost - p2->cost);
 }
-
-
-/*  Delete all data created  */
-void deleteData(int tam){
-    int i;
-    Pphoto item, next;
-    for (i = 0; i < tam; i++){
-        for(item = graph[i]; item != NULL; item = next ){
-            next = item->next;
-            free(item);
-        }
-    }
-    free(graph);
-    free(order);
-}
-
-
-/*  Insert new photo on the graph  */
-void insertPhoto(int source, int destiny){
-    Pphoto new = newPhoto(destiny);
-    new->next = graph[source - 1]->next;
-    graph[source - 1]->next = new;
-}
-
-
-/*  Create new photo  */
+/*  Create new connection  */
 Pconnection newConnection (int city1, int city2, int cost){
     Pconnection new = (Pconnection) malloc(sizeof(struct connection));
     new->city1 = city1;
@@ -119,3 +124,7 @@ Pconnection newConnection (int city1, int city2, int cost){
     new->cost = cost;
     return new;
 }
+
+
+
+
